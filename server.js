9c70e742937626ded312d280590b1a151aff19d3
DevYeop <lee.sangyeop0715@gmail.com>
2026-01-05 11:24:52 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+
+const app = express();
+const PORT = 4000; // 어디방
+
+// GET / 요청이 오면
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/api/data', (req, res) => {
+  res.send('data~');
+});
+
+app.get('/get_test', (req, res) => {
+  res.send('test');
+});
+
+// 서버 실행
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
